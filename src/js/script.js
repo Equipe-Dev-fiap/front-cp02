6d6fa531bde47dcafd97cb618b9041f1dbf92ba2
eduardo <eduardo@marcotec.com.br>
2025-05-16 20:47:29 -0300

    adicionando script javascript

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+// Smooth scroll for anchor links (not needed on multi pages but kept for future use)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+  
